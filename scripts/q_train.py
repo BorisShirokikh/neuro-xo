a8b8dbfb9e917a8b482a77a27d43c70ec25338b2
Boris Shirokikh <bg.dustpelt@gmail.com>
2021-10-24 12:48:49 +0300

    FIX: upd q_train script

--- scripts/q_train.py
+++ scripts/q_train.py
@@ -7,15 +7,15 @@ from torch.utils.tensorboard import SummaryWriter
 from dpipe.io import choose_existing
 from dpipe.torch import save_model_state, load_model_state
 
-from ttt_lib.q_learning import PolicyNetworkQ, PolicyPlayer, train_q_learning, PolicyNetworkQ6, PolicyNetworkQ10, \
-    PolicyNetworkQ10Light
+from ttt_lib.torch.module.policy_net import PolicyNetworkQ3, PolicyNetworkQ6, PolicyNetworkQ10, PolicyNetworkQ10Light
+from ttt_lib.q_learning import PolicyPlayer, train_q_learning
 from ttt_lib.field import Field
 
 
 if __name__ == '__main__':
 
     Q_EXP_PATH = choose_existing(
-        Path('/nmnt/x2-hdd/experiments/rl/'),
+        Path('/nmnt/x4-hdd/experiments/rl/'),
         Path('/experiments/borish/RL/ttt/mces')
     )
 
@@ -29,12 +29,15 @@ if __name__ == '__main__':
 
     parser.add_argument('--light_model', required=False, action='store_true', default=False)
 
+    parser.add_argument('--duel_name', required=False, type=str, default=None)
     parser.add_argument('--preload_path', required=False, type=str, default=None)
 
     args = parser.parse_known_args()[0]
 
     exp_path = Q_EXP_PATH / args.exp_name
     os.makedirs(exp_path, exist_ok=True)
+
+    duel_path = None if args.duel_name is None else Q_EXP_PATH / args.duel_name
 
     log_dir = exp_path / 'logs'
     log_dir.mkdir(exist_ok=True)
@@ -46,13 +49,14 @@ if __name__ == '__main__':
     # hyperparameters:
     n = args.field_size
     kernel_len = args.win_len
+    lr = 1e-2
     device = 'cuda'
 
     field = Field(n=n, kernel_len=kernel_len, device=device, check_device=device)
 
     cnn_features = (128, 64)
     if n == 3:
-        model = PolicyNetworkQ(n=n, structure=cnn_features)
+        model = PolicyNetworkQ3(n=n, structure=cnn_features)
     elif n == 6:
         model = PolicyNetworkQ6(n=n, structure=cnn_features)
     elif n == 10:
@@ -67,15 +71,16 @@ if __name__ == '__main__':
     if preload_path is not None:
         load_model_state(model, preload_path)
 
-    n_episodes = int(1e6)
+    n_episodes = 600000
     eps_init = 0.5
-    ep2eps = {int(1e5): 0.4, int(2e5): 0.35, int(3e5): 0.3, int(4e5): 0.25, int(5e5): 0.2,
-              int(8e5): 0.15, int(9e5): 0.1}
+    ep2eps = {int(1e5): 0.4, int(2e5): 0.3, int(3e5): 0.3, int(4e5): 0.2, int(5e5): 0.1, }
 
     player = PolicyPlayer(model=model, field=field, eps=eps_init, device=device)
 
     # TRAIN:
-    train_q_learning(player, logger, exp_path, n_episodes=n_episodes, n_step_q=n_step_q, ep2eps=ep2eps, n_duels=100)
+    train_q_learning(player, logger, exp_path, n_episodes, augm=True, n_step_q=n_step_q,
+                     ep2eps=ep2eps, lr=lr,
+                     episodes_per_epoch=10000, n_duels=100, episodes_per_model_save=10000,
+                     duel_path=duel_path)
     # SAVE:
-    # save(sa2g, exp_path / 'sa2g.json')
     save_model_state(player.model, exp_path / 'model.pth')
