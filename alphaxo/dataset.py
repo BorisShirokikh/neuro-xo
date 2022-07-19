a9840d93cf7817b732bf1eb6b6f17b791fbf129e
Alexey Shevtsov <aalexshevtsov@gmail.com>
2022-07-19 22:08:38 +0300

    dataset draft

---
+++ alphaxo/dataset.py
@@ -0,0 +1,19 @@
+from torch.utils.data import Dataset
+
+
+class DatasetXO(Dataset):
+    def __init__(self, ):
+        # nested numpy arrays of size (n, n), each represents the particular position
+        self.s = []
+
+        # nested numpy arrays of size (n * n,) each represents the probability of action in the corresponding position
+        self.pi = []
+
+        # list of the outcome from the first player's point of view in the corresponding position
+        self.z = []
+
+    def __getitem__(self, index):
+        return self.s[index], self.pi[index], self.z[index]
+
+    def __len__(self):
+        return len(self.s)
