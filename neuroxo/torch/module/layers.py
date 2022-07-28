a9e6b3d8855aa3ac9a23655fd9664bb5f79c07b5
Boris Shirokikh <bg.dustpelt@gmail.com>
2022-07-28 15:31:17 +0300

    FEAT: rewrite proba-policy net

--- neuroxo/torch/module/layers.py
+++ neuroxo/torch/module/layers.py
@@ -1,26 +1,5 @@
-import math
-
 import torch
 import torch.nn as nn
-
-
-class Bias(nn.Module):
-    def __init__(self, n_channels, device=None, dtype=None) -> None:
-        factory_kwargs = {'device': device, 'dtype': dtype}
-        super(Bias, self).__init__()
-        self.n_channels = n_channels
-        self.bias = nn.Parameter(torch.empty(n_channels, **factory_kwargs))
-        self.reset_parameters()
-
-    def reset_parameters(self) -> None:
-        bound = 1 / math.sqrt(self.n_channels)
-        nn.init.uniform_(self.bias, -bound, bound)
-
-    def forward(self, input: torch.Tensor) -> torch.Tensor:
-        return input + self.bias
-
-    def extra_repr(self) -> str:
-        return f'n_channels={self.n_channels}, bias={self.bias}'
 
 
 class MaskedSoftmax(nn.Softmax):
--- neuroxo/torch/module/policy_net.py
+++ neuroxo/torch/module/policy_net.py
@@ -1,36 +1,24 @@
 import torch
 import torch.nn as nn
-from dpipe.layers import ResBlock2d
+from dpipe.layers import ResBlock2d, PostActivation2d
 from dpipe.torch import to_device
 
-from neuroxo.torch.module.layers import Bias, MaskedSoftmax
+from neuroxo.torch.module.layers import MaskedSoftmax
 
 
-class PolicyNetworkQ10(nn.Module):
-    def __init__(self, structure=None, n=10, in_channels=5):
+class ProbaPolicyNN(nn.Module):
+    def __init__(self, n: int = 10, in_channels: int = 2, n_blocks: int = 11, n_features: int = 128):
         super().__init__()
-        if structure is None:
-            structure = (128, 64)
-        self.structure = structure
         self.n = n
         self.in_channels = in_channels
+        self.n_blocks = n_blocks
+        self.n_features = n_features
 
-        n_features = structure[0]
-        n_features_policy = structure[1]
         self.model = nn.Sequential(
             nn.Conv2d(in_channels, n_features, kernel_size=3, padding=1, bias=False),
-            # ResBlock actually starts with BN -> ReLU
-            # nn.BatchNorm2d(n_features),
-            # nn.ReLU(inplace=True),
-            ResBlock2d(n_features, n_features, kernel_size=3, padding=1),
-            ResBlock2d(n_features, n_features, kernel_size=3, padding=1),
-            ResBlock2d(n_features, n_features, kernel_size=3, padding=1),
-            ResBlock2d(n_features, n_features, kernel_size=3, padding=1),
-            ResBlock2d(n_features, n_features, kernel_size=3, padding=1),
-            # policy head
-            nn.Conv2d(n_features, n_features_policy, kernel_size=1, padding=0, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(n_features_policy, 1, kernel_size=1, padding=0, bias=True),
+            nn.Sequential(*[ResBlock2d(n_features, n_features, kernel_size=3, padding=1) for _ in range(n_features)]),
+            PostActivation2d(n_features, n_features, kernel_size=1, padding=0),
+            nn.Conv2d(n_features, 1, kernel_size=1, padding=0, bias=True),
         )
 
         self.tanh = nn.Tanh()
@@ -38,17 +26,14 @@ class PolicyNetworkQ10(nn.Module):
         self.masked_softmax = MaskedSoftmax(dim=1)
 
     def forward(self, x):
-        return self.model(x)
-
-    def predict_action_values(self, logits):
-        return self.tanh(logits)
-
-    def predict_proba(self, x, logits):
-        p = self.masked_softmax(self.flatten(logits), self.flatten(x[:, -1, ...].unsqueeze(1)))
-        return torch.reshape(p, shape=(-1, 1, self.n, self.n))
+        logits = self.model(x)
+        policy = self.tanh(logits)
+        proba = self.masked_softmax(self.flatten(logits), self.flatten(torch.sum(x[:, :2, ...], dim=1).unsqueeze(1)))
+        return proba, policy
 
 
-class PolicyNetworkRandom(nn.Module):
+class RandomProbaPolicyNN(nn.Module):
+    # FIXME: should actually generate and return random variables
     def __init__(self, n=10, in_channels=5):
         super().__init__()
         self.n = n
